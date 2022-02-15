f99740e0735c731cb2d0386f8ae55577b4efe4ae
Nguyen Tien Tai <anhtaibnvn12112001@gmail.com>
2022-02-15 11:42:28 +0700

    fix

--- view/giangvien/QLHocSinhTheoMonHoc.php
+++ view/giangvien/QLHocSinhTheoMonHoc.php
@@ -60,7 +60,16 @@
                                     });
                                 });
                             </script>
-                            <option class="a" id="Tất cả">Tất cả</option>
+                            <?php 
+                                if($mon == []){
+                                    echo '<option class="a" id="">Chưa được phân công</option>';
+                                }
+                                else{
+                                    echo '<option class="a" id="Tất cả">Tất cả</option>';
+                                }
+                             ?>
+                           
+                           
                             <?php foreach ($mon as $monhoc) { ?>
                                 <option class="a" id="<?= $monhoc['tenmon'] ?>"><?php echo $monhoc['tenmon']; ?></option>
                             <?php } ?>
