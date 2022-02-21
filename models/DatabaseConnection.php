549075d7748370717c7eb99276ee3d361efdf2b2
Nguyen Tien Tai <anhtaibnvn12112001@gmail.com>
2022-02-21 12:46:48 +0700

    fix code

--- models/DatabaseConnection.php
+++ models/DatabaseConnection.php
@@ -16,19 +16,7 @@ final class DatabaseConnection {
     private function __construct() {}
     
     private function __clone() {}
-    // function executeResult($sql){
-    //     $conn = mysqli_connect($this->hostname, $this->username, $this->pass, $this->dbname);
-        
-    //     $result = mysqli_query($conn, $sql);
-    //     $list = [];
-    //     while ($row = mysqli_fetch_array($result, 1)) {
-    //         $list[] = $row;
-    //     }
-    //     mysqli_close($conn);
-        
-    //     return $list;
-    // }
-
+    
     
     // private function __wakeup() {}
     
@@ -41,14 +29,7 @@ final class DatabaseConnection {
         // $this->conn = new mysqli($this->hostname, $this->username, $this->pass, $this->dbname);
 
         
-    //    if(!self::$connection){
-    //        echo "Kết nối thất bại";
-    //         exit();
-    //    }else{
-    //         echo "Kết nối thanh cong";
-
-    //     //    mysqli_set_charset(self::$connection,'utf8');
-    //    }
+    
        return self::$connection;
     }
     
